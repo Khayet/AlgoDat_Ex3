d271c71e03443f422eee5d9f04f000db9254d727
Khayet <Khayet@users.noreply.github.com>
2016-01-06 20:53:21 +0100

    NOT WORKING: implement kmp

--- kmp.cpp
+++ kmp.cpp
@@ -1,18 +1,74 @@
 #include <iostream>
 #include <string>
+#include <vector>
 
-bool match_string(std::string const& pat, std::string const& text, int& pos) {
-	
-	int j{0};
-	for (int i=0; i < text.length(); ++i) {
-		if (text.substr(j, j+pat.length()) == pat) {
-			pos = j;
-			return true;
+void print_table(std::vector<int>& table) {
+	for (auto i : table) {
+		std::cout << i << ", ";
+	}
+	std::cout << std::endl;
+}
+
+void fill_table(std::string const& word, std::vector<int>& table) {
+	// refer to: https://en.wikipedia.org/wiki/Knuth%E2%80%93Morris%E2%80%93Pratt_algorithm
+	int pos{2}, index{0};
+
+	table[0] = -1;
+	table[1] = 0;
+
+	std::cout << "Still working" << std::endl;
+
+	while (pos < word.length()) {
+		if (word[pos-1] == word[index]) {
+			table[pos] = index + 1;
+			++index;
+			++pos;
+		} else if (index > 0) {
+			index = table[index];
 		} else {
-			j += pat.length();
-			std::cout << j << "\n";
+			std::cout << "pos = " << pos << std::endl;
+			table[pos] = 0;
+			std::cout << "table[2] = " << table[2] << std::endl;
+			++pos;
 		}
 	}
+
+	print_table(table);
+}
+
+bool match_string(std::string const& pat, std::string const& text, int& pos) {
+	std::vector<int> next(pat.length());
+	std::cout << "Still working" << std::endl;
+	fill_table(pat, next);
+
+	std::cout << "Still working" << std::endl;
+
+	int i{0}, j{0};
+	while(i+j < text.length()) 
+	{
+		std::cout << "Still working; i = " << i << std::endl;
+		if (pat[j] == text[i+j]) {
+			if (j == pat.length()-1) {
+				pos = i;
+				return true;
+			}
+			++j;
+		} else {
+			if (next[i] > -1) {
+				std::cout << "j = " << j << std::endl;
+				i = i + j - next[j];
+				std::cout << "j = " << j << std::endl;
+				j = next[j];
+				std::cout << "next[j] = " << next[j] << std::endl;
+			} else {
+				j = 0;
+				++i;
+			}
+		}
+	}
+
+	std::cout << "Still working" << std::endl;
+	
 	return false;
 }
 
